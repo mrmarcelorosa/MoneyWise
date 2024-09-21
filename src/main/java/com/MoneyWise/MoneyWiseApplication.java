8ca4b1dadaa190ce52d14c9dca73ad09a56f8104
Marcelo Rosa <vyctor.rosa14@gmail.com>
2024-09-21 18:37:39 -0300

    Initial commit

---
+++ src/main/java/com/MoneyWise/MoneyWiseApplication.java
@@ -0,0 +1,13 @@
+package com.MoneyWise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyWiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyWiseApplication.class, args);
+	}
+
+}
